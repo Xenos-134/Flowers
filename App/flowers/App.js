45034e9488138e705021c82cd3dda9e667e81923
Xenos <artemmordan@gmail.com>
2022-07-24 03:02:00 +0100

    [ADDED]: Buttons

--- App/flowers/App.js
+++ App/flowers/App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Pressable } from 'react-native';
 import { Dimensions } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { useState, useEffect } from 'react';
 import FormData from 'form-data'
+import { Shadow } from 'react-native-shadow-2';
 
 const SCREEN = Dimensions.get("screen");
 var axios = require('axios');
@@ -13,13 +14,14 @@ export default function App() {
   const [image, setImage] = useState(null);
   const [form_data, setForm] = useState(null);
   const [status, requestPermission] = ImagePicker.useCameraPermissions();
+  const [prediction, setPrediction] = useState(null);
 
 
 
   async function sendImage() {
 
     console.log(form_data)
-    axios({
+    await axios({
       method: "post",
       //url: SERVER_ADDR+"/item/newCar",
       url: "http://192.168.1.224:3000/evaluate",
@@ -27,8 +29,10 @@ export default function App() {
       headers: {
           "Content-Type": "multipart/form-data", 
       }
-
-  })
+    }).then(res => {
+      console.log("RECEIVED RESPONSE: ",res.data);
+      setPrediction(res.data);
+    })
   }
 
 
@@ -71,25 +75,48 @@ export default function App() {
     }
   };
 
+
+
   return (
     <View style={styles.container}>
       <Text>Image classification of flowersusing tensorflow </Text>
-      <View style={styles.image_view}>
-        {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
-        <Text>IMAGE</Text>
+      <View style={{marginVertical: 20}}>
+        <Shadow  offset={[3, 3]} distance={10} startColor="#727272" radius={30}>
+          <Shadow offset={[-3, -2]} distance={10} startColor="#ffffff" radius={30}>
+              <View style={styles.image_view}>
+                {image && <Image source={{ uri: image }} style={{ width: SCREEN.width * 0.7, height: SCREEN.width*0.7, borderRadius: 30 }} />}
+              </View>
+          </Shadow>
+        </Shadow>
       </View>
-      <View style={styles.predictions_view}>
-        <Text>
-          PREDICTIONS
-        </Text>
-      </View>
-      <View style={styles.buttons_view}>
-        <Button title="SELECT" onPress={pickImage} />
-        <Button title="SEND" onPress={sendImage} />
+
+      <Shadow  offset={[3, 3]} distance={10} startColor="#727272" radius={30}>
+        <Shadow offset={[-3, -2]} distance={10} startColor="#ffffff" radius={30}>
+          <View style={styles.predictions_view}>
+            {!prediction?<Text>NO PREDICTIONS</Text>:<Text>{prediction}</Text>}
+          </View>
+        </Shadow>
+      </Shadow>
+      <View style={{marginTop: 30}}>
+        <Shadow  offset={[3, 3]} distance={10} startColor="#727272" radius={30}>
+          <Shadow offset={[-3, -2]} distance={10} startColor="#ffffff" radius={30}>
+            <View style={styles.buttons_view}>
+              <Pressable title="SELECT" onPress={pickImage} style={styles.button}>
+                <Text>SELECT</Text>
+              </Pressable>
+              <Pressable title="SEND" onPress={sendImage} style={[styles.button, {backgroundColor: "green"}]}>
+                <Text>CLASSIFY</Text>
+              </Pressable>
+            </View>
+          </Shadow>
+        </Shadow>
       </View>
     </View>
   );
 }
+
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -97,27 +124,41 @@ const styles = StyleSheet.create({
     backgroundColor: '#e0e0e0',
     alignItems: 'center',
     justifyContent: 'flex-start',
-    paddingTop: "10%"
+    paddingTop: "10%",
   },
   image_view: {
-    backgroundColor: "red",
-    width: SCREEN.width * 0.95,
-    height: SCREEN.width * 0.95
+    width: SCREEN.width * 0.8,
+    height: SCREEN.width * 0.8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#e0e0e0",
+    borderRadius: 30,
   },
   predictions_view: {
     width: SCREEN.width * 0.9,
     height: SCREEN.width * 0.5,
-    backgroundColor: "blue",
-    marginTop: 10,
+    backgroundColor: "#e0e0e0",
+    borderRadius: 30,
+    alignItems: "center",
+    justifyContent: "center"
   },
   buttons_view : {
-    marginTop: 10,
-    backgroundColor: "yellow",
-    width: SCREEN.width * 0.8,
+    backgroundColor: "#e0e0e0",
+    width: SCREEN.width * 0.9,
     height: SCREEN.height * 0.15,
     justifyContent: "space-evenly",
+    alignItems: "center",
     flexDirection: "row",
-    padding: 20
+    padding: 20,
+    borderRadius: 30,
+  },
+  button: {
+    width:  SCREEN.width * 0.35,
+    height:  SCREEN.width * 0.15,
+    backgroundColor: "red",
+    alignItems:"center",
+    justifyContent:"center",
+    borderRadius: 20
   }
   
 });
